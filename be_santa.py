e3e57569b7e1bbf11dbe9b52c0566c9fe0ba4c5e
David McFadden <podll0711@gmail.com>
2021-01-15 13:28:33 -0600

    add hotel manager

---
+++ be_santa.py
@@ -0,0 +1,8 @@
+#user's input
+presents = input('What do you want for Christmas? ')
+naughty_nice = input('Have you been naughty of nice?')
+
+if naughty_nice == 'nice':
+    print('For Christmas your\'re getting, ' + presents)
+else:
+    print('You\'re getting coal this year buddy!')
--- subsequence.py
+++ subsequence.py
@@ -1,3 +1,4 @@
 def isValidSubsequence(array, sequence): 
+    if isValidSubsequence
 #make sure to add both array and sequence as arguements.
     print(isValidSubsequence([1,2,3,4], [2,4]))
